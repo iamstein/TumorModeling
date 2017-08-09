1e0b92e48d34f46124507ec1cba2c23c7eac5539
Miandra Ellis <miandraann@gmail.com>
2017-08-08 19:31:27 -0700

    Added more comments. Changed graphs to print in loop.

--- pgm_TumorModel_R/Task10_sensitivity.R
+++ pgm_TumorModel_R/Task10_sensitivity.R
@@ -1,5 +1,5 @@
 # Setup.
-setwd("~/GitHub/TumorModeling/pgm_TumorModel_R")
+setwd("~/TumorModeling/pgm_TumorModel_R")
 source("ams_initialize_script.R")
 dirs = get.dirs(sys.calls(),dirs)
 theme_set(theme_classic())
@@ -7,31 +7,27 @@ theme_set(theme_classic())
 # Create fold change function.
 lseq = function(from,to,length.out){exp(seq(log(from), log(to), length.out = length.out))}
 
-# Scale Transformations.
-scale.nmol2mpk = 1e-9*150e3/1000*70 #nM->mg/kg
-scale.mpk2nmol = 1/scale.nmol2mpk
-
 # Load data and model.
-mod      = ivsc_3cmtct_full()
-pin        = xlsx::read.xlsx("../data/Bx_DTN_example.xlsx",1)
-# Assumption made for model C. Comment if model changes to F.
+mod = ivsc_3cmtct_full()
+pin = xlsx::read.xlsx("../data/Bx_DTN_example.xlsx",1)
+# Assumption made for model C. Comment out if model changes to F.
 pin[14:17,3] = 0
 
 # Create parameter vector.
 p = pin$Value
 names(p) = pin$Parameter
 
-# Specify ranges of parameters to explore
+# Specify parameters to explore
 param   = c('kon3', 'koff3','dose','keD1','k13D','keDT3')
 units   = c('1/(nM*d)','1/(nM*d)','mg','1/d','1/d','1/d')
 nparam  = length(param)
-order   = 1:nparam             #order for parameters to be plotted
-title.scale = c(rep(1,nparam)) #in case one wants to change units in title
+order   = 1:nparam             # order for parameters to be plotted
+title.scale = c(rep(1,nparam)) # in case one wants to change units in title
 
 # Setup and exploration data.frame to iterate through for each of the analysis to perform.
 explore = data.frame(param = param, units=units, title.scale=title.scale, order=1:nparam,
-fold.min = .001, fold.max = 10, fold.n   = 6,
-stringsAsFactors = FALSE)
+  fold.min = .01, fold.max = 10, fold.n   = 5,
+  stringsAsFactors = FALSE)
 explore = explore[explore$order,]
 row.names(explore) = param
   
@@ -39,9 +35,9 @@ row.names(explore) = param
 p["dose"]   = 420 #mg
 p["tau"]    = 1   #dosing interval
 ev = list(
-t.sample = c(1e-4,1e-3,1e-2,seq(.1,6*7,.1)), #d
-n.dose   = 1,
-cmt      = which(mod$cmtshort=="D1")
+  t.sample = c(1e-4,1e-3,1e-2,seq(.1,6*7,.1)), #d
+  n.dose   = 1,
+  cmt      = which(mod$cmtshort=="D1")
 )
 
 # Simulate model and put into OUT
@@ -50,13 +46,16 @@ p0    = p
 OUT   = data.frame()
 
 for (par in explore$param) {
+  # Changes which parameter is currently being explored.
   ex = explore[par,]
   foldchange = lseq(ex$fold.min,ex$fold.max,ex$fold.n)
   for (foldpar in foldchange) {
     # Set up parameters
+    # Set up local verion of parameter list for exploration.
     p      = p0
     p[par] = p0[par]*foldpar
     T_30 = p["koff3"]/p["kon3"]
+    
     # Set up dosing (where dose and dosing interval are parameters that can vary)
     ndose     = 1
     event     = eventTable(amount.units="nmol", time.units = "days")
@@ -64,7 +63,7 @@ for (par in explore$param) {
     event$add.dosing(dose=p["dose"]/ndose*scale.mpk2nmol,nbr.doses=4*ndose,
                      dosing.interval=21/ndose,dosing.to=2)
     
-    # Solve the ODEs.
+    # Solve the ODEs using RxODE.
     out      = mod$rxode$solve(p, event, mod$init(p))
     out      = mod$rxout(out,p)
  
@@ -82,17 +81,18 @@ for (par in explore$param) {
     # Append to data.frame()
     OUT = rbind(OUT,out)
   } # end param[i]
-  
+
 }# end param list
-g1 = ggplot(data=OUT,aes(x=time,y=Dtot1,group=label.ind, color=pfold)) + geom_line() + 
-    facet_grid(~label.group) + scale_y_log10() + 
-    scale_colour_gradient2(trans="log", guide="legend",low="blue", mid = "purple", high="red",breaks=c(10,1,.1))
-g2 = ggplot(data=OUT,aes(x=time,y=Ttot1,group=label.ind, color=pfold)) + geom_line() + 
-    facet_grid(~label.group) + scale_y_log10() + 
-    scale_colour_gradient2(trans="log", guide="legend",low="blue", mid = "purple", high="red",breaks=c(10,1,.1))
-g3 = ggplot(data=OUT,aes(x=time,y=ratio,group=label.ind, color=pfold)) + geom_line() + 
-    facet_grid(~label.group) + scale_y_log10() + 
-    scale_colour_gradient2(trans="log", guide="legend",low="blue", mid = "purple", high="red",breaks=c(10,1,.1))
-print(g1)
-print(g2)
-print(g3)
+
+# Graphing.
+theme = theme(text = element_text(size = 16))
+
+# Vector of which variables from OUT you would like to be plotted.
+plotme = c("Dtot1", "Ttot1", "ratio")
+
+for (feature in plotme){
+    # aes_string needed to do this in a loop
+    print(ggplot(data=OUT,aes_string(x="time",y=feature,group="label.ind", color="pfold")) + 
+        geom_line() + facet_grid(~label.group) + scale_y_log10() + theme + 
+        scale_colour_gradient2(trans="log", guide="legend",low="blue", mid = "purple", high="red",breaks=c(10,1,.1)))
+}
